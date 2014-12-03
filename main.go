6eec9f1f40a6a0f42f00f279ad99c162d1e61e82
jiami <winters.mi@gmail.com>
2014-12-03 15:26:35 +0800

    Basic flag parsing

--- main.go
+++ main.go
@@ -1,10 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	fmt.Println("ModelQ")
+	var targetDb, tableName, packageName string
+	var cleanPackage bool
+	flag.StringVar(&targetDb, "db", "", "Target database source string: e.g. root@tcp(127.0.0.1:3306)/test?charset=utf-8")
+	flag.StringVar(&tableName, "tables", "", "You may specify which tables the models need to be created")
+	flag.BoolVar(&cleanPackage, "clean", false, "If clean the generated code directory")
+	flag.StringVar(&packageName, "pkg", "", "Go source code package for generated models")
+	flag.Parse()
+
+	if targetDb == "" {
+		fmt.Println("Please provide the target database source.")
+		fmt.Println("Usage:")
+		flag.PrintDefaults()
+		return
+	}
+	if packageName == "" {
+		printUsages("Please provide the go source code package name for generated models.")
+		return
+	}
+
+	cfg, err := parseDsn(targetDb)
+	if err != nil {
+		printUsages("The target database source string doesn't seem correct...", err)
+		return
+	}
+	if cfg.dbname == "" {
+		printUsages("Please provide the target database name.")
+		return
+	}
+
+	fmt.Println(cfg)
+}
+
+func printUsages(message ...interface{}) {
+	for _, x := range message {
+		fmt.Println(x)	
+	}
+	fmt.Println("\nUsage:")
+	flag.PrintDefaults()
 }
